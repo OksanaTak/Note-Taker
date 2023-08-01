4160d3c9817cb3f03ca77db2d2adc3323dc93a6e
Oksana Tatsyak <oksana.tatsyak@gmail.com>
2023-08-01 08:55:06 -0700

    Wrote code for apiroutes.js file

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,1 +1,25 @@
 
+const app = require("express").Router()
+//db
+let db=require("../db/db.json") ;
+const fs = require("fs")
+
+app.get("/notes",function(req,res){
+    db = JSON.parse(fs.readFileSync("./db/db.json")) || []
+    res.json(db)
+})
+app.post("/notes",function(req,res){
+   let userNote = {
+    title: req.body.title,
+    text:req.body.text,
+    id:Math.floor(Math.random() * 9999)
+   }
+   db.push(userNote)
+   fs.writeFile("./db/db.json",JSON.stringify(db),function(err){
+    if(err) throw err;
+   })
+    res.json(db)
+})
+
+
+module.exports = app

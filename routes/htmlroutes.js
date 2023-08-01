e0241ffad769fe5d95bdc80885fd32f92979e414
Oksana Tatsyak <oksana.tatsyak@gmail.com>
2023-08-01 08:33:01 -0700

    Completed HTML Routes and created server.js file

---
+++ routes/htmlroutes.js
@@ -0,0 +1,17 @@
+const app = require('express').Router()
+const path = require('path')
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+)
+
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
+)
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+)
+module.exports=app

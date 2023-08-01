99f59fdd34243a099fe8256a6d0f3438b69921bb
Oksana Tatsyak <oksana.tatsyak@gmail.com>
2023-08-01 08:42:25 -0700

    Updated server.js file

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
-
 app.use(express.static('public'));
-
+app.use(routes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
